3697f49867671455962e0e1dd4bd95e5755a3b75
isaicalderon <isai.acalderon@gmail.com>
2019-02-27 23:08:24 -0700

    update 2

--- app/src/main/java/com/example/lbeltran/dia1/ContactListAdapter.java
+++ app/src/main/java/com/example/lbeltran/dia1/ContactListAdapter.java
@@ -56,11 +56,11 @@ public class ContactListAdapter extends BaseAdapter {
         TextView c_likes =(TextView)view.findViewById(R.id.contact_likes);
         TextView c_comments =(TextView)view.findViewById(R.id.contact_comments);
 
-        c_id.setText(contacto.getId());
-        c_name.setText(contacto.getNombreCompleto());
-        c_likes.setText(String.valueOf(contacto.getLikes()));
-        c_comments.setText(String.valueOf(contacto.getComments()));
-
+        c_id.setText("ID: "+contacto.getId());
+        c_name.setText("Nombre: "+contacto.getNombreCompleto());
+        c_likes.setText("Likes: "+String.valueOf(contacto.getLikes()));
+        c_comments.setText("Comments: "+String.valueOf(contacto.getComments()));
+        
         Picasso.with(activity).load(contacto.getUrlFoto()).placeholder(R.mipmap.ic_launcher)
                 .error(R.mipmap.ic_launcher)
                 .into(c_pic, new com.squareup.picasso.Callback(){
